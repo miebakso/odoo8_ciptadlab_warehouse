679d9949d42950cd039402a064713a09de044650
miebakso <matthew_ariel_wangsit@yahoo.com>
2018-09-21 06:56:18 +0700

    done

---
+++ __openerp__.py
@@ -0,0 +1,27 @@
+{
+	'name': 'Francisee',
+	'description': 'Francisee modul',
+	'category': 'Development',
+	'version': '1.0.0',
+	'author': 'Test',
+	'maintainer': 'Matthew Ariel, Edrick Ezra, Stephen Senjaya, Jonathan Alva',
+	'website': 'https://iaaaa.asd',
+	'sequence': 150,
+	'depends': [
+		'base_setup',
+		'product',
+		'account',
+		'base'
+	],
+	'data': [
+		'views/franchisee_view.xml',
+	],
+	'qweb': [
+	],
+	'demo': [
+	],
+	'test': [
+	],
+	'auto_install': False,
+	'installable': True,
+}
